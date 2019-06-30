dbb8f5adaad40082c16f7c30b1bc84ef1c82b862
curlson <814801076@qq.com>
2019-06-30 14:38:40 +0000

    feat: 修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,7 +5,7 @@
             LaraBBS
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -18,8 +18,29 @@
             {{-- Right Side Of Navbar --}}
             <ul class="navbar-nav navbar-right">
                 {{-- Authentication Links --}}
-                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
-                <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+                @guest
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
+                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" alt="" width="30px" height="30px">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a href="" class="dropdown-item">个人中心</a>
+                            <a href="" class="dropdown-item">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item" id="logout">
+                                <form action="{{ route('logout') }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                </form>
+                            </a>
+                        </div>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
