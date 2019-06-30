44e9c6b3cd7d7d066431416bf07d9f37dcf1111c
curlson <814801076@qq.com>
2019-06-30 10:57:26 +0000

    feat: 新增自定义辅助函数文件

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,20 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: idmin
+ * User: freelancer
  * Date: 2019/6/29
  * Time: 18:24
  */
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * 根据 route 将 container 定义制定的 class 类
+ *
+ *  <div id="app" class="{{ route_class() }}-page">
+ */
+function route_class()
+{
+    $currentRouteName = Route::currentRouteName();
+    return str_replace('.', '-', $currentRouteName);
+}
