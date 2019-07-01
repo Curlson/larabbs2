d81b570d1a761ea57607619c0512a80f7b87eeb0
sunrui <18556368687@163.com>
2019-07-01 18:03:28 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -10,6 +10,7 @@ namespace App\Handlers;
 
 
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ImageUploadHandler
@@ -23,7 +24,7 @@ class ImageUploadHandler
      * @param $file_prefix
      * @return array|bool
      */
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width)
     {
         // 构建存储的文件夹规则，值如： uploads/images/avatars/201909/21/
         // 文件夹切割能让查找效率更高。
@@ -48,8 +49,32 @@ class ImageUploadHandler
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $file_name);
         
+        // 如果限制了图片快递, 就进行裁剪
+        if($max_width && $extension != 'git') {
+            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+        }
+        
         return [
             'path' => config('app.url') . "/{$folder_name}/{$file_name}"
         ];
+    }
+    
+    private function reduceSize(string $file_path, $max_width)
+    {
+        // 先治理华, 传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+        
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+            
+            // 设定宽度是 $max_width, 高度等比例双方缩放
+            $constraint->aspectRatio();
+            
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+        
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
