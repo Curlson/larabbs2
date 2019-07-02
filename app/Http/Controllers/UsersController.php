e1bea3006b933703fd1b1527358add6af303397d
sunrui <18556368687@163.com>
2019-07-02 09:11:31 +0800

    feat: 授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+    
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,18 +22,22 @@ class UsersController extends Controller
     
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     
     public function update(UserUpdateRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->validated();
+        
         if($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if($result) {
                 $data['avatar'] = $result['path'];
             }
         }
+        
         $user->update($data);
         return redirect()->route('users.show', Auth::id())->with('success', '个人信息修改成功 ^_^');
     }
